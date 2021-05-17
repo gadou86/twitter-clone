9986b01610e4a0a1b43cd7942588b168d98056b9
Gad Dayan <70294896+gadou86@users.noreply.github.com>
2021-05-17 13:52:31 -0400

    cleanded up unnnasasary files and added sidebar.js

---
+++ src/SideBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Sidebar.css'
+
+function SideBar() {
+  return (
+    <div className="sidebar">
+      {/* Twitter icon */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+
+
+      {/* Button -< Tweet */}
+    </div>
+  )
+}
+
+export default SideBar
