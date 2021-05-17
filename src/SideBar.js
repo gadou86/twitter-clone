129300c8e3a0eae2f81426c7125f1a1ef4031ada
Gad Dayan <70294896+gadou86@users.noreply.github.com>
2021-05-17 17:51:39 -0400

    added sidebar and styled it

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,19 +1,36 @@
 import React from 'react'
 import './Sidebar.css'
+import TwitterIcon from '@material-ui/icons/Twitter';
+import SidebarOption from './SidebarOption'
+import HomeIcon from '@material-ui/icons/Home';
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import PermIdentityIcon from '@material-ui/icons/PermIdentity';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import SearchIcon from '@material-ui/icons/Search';
+import { Button } from '@material-ui/core';
 
 function SideBar() {
   return (
     <div className="sidebar">
       {/* Twitter icon */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
+      <TwitterIcon />
 
+      <SidebarOption active Icon={HomeIcon} text="Home"/> 
+      <SidebarOption Icon={SearchIcon} text="Explore"/> 
+      <SidebarOption Icon={NotificationsNoneIcon} text="Notifications"/> 
+      <SidebarOption Icon={MailOutlineIcon} text="Messages"/> 
+      <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks"/> 
+      <SidebarOption Icon={ListAltIcon} text="Lists"/> 
+      <SidebarOption Icon={PermIdentityIcon} text="Profile"/> 
+      <SidebarOption Icon={MoreHorizIcon} text="More"/> 
 
       {/* Button -< Tweet */}
+      <Button variant="outlined" className="sidebar-tweet" fullWidth>
+        Tweet
+      </Button>
     </div>
   )
 }
