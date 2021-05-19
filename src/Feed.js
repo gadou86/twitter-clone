b8df528957972bb0999a6c941e1a76644f16ec4e
Gad Dayan <70294896+gadou86@users.noreply.github.com>
2021-05-19 13:16:11 -0400

    added widgets

--- src/Feed.js
+++ src/Feed.js
@@ -2,6 +2,8 @@ import React from 'react'
 import './Feed.css'
 import TweetBox from './TweetBox'
 import Post from './Post'
+import { Timeline } from 'react-twitter-widgets'
+
 
 function Feed() {
   return (
@@ -15,6 +17,12 @@ function Feed() {
       {/* TweetBox */}
     <TweetBox />
       <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+
     </div>
   )
 }
--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import './Widgets.css'
+import SearchIcon from '@material-ui/icons/Search';
+import { TwitterTweetEmbed, TwitterShareButton, TwitterTimelineEmbed } from 'react-twitter-embed'
+import { Timeline, Tweet } from 'react-twitter-widgets'
+
 
 function Widgets() {
   return (
     <div className="widgets">
-      <h2>Widgets</h2>
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input type="text" placeholder="Search Twitter"/>
+      </div>
+
+      <div className="widgets_widgetContainer">
+        <h2>What's happening</h2>
+
+        <Tweet tweetId="841418541026877441" />
+        <Timeline
+              dataSource={{
+                sourceType: 'profile',
+                screenName: 'Gadmtl86'
+              }}
+              options={{
+                height: '400'
+              }}
+            />
+        <TwitterTweetEmbed tweedId={"841418541026877441"}/>
+        
+      </div>
     </div>
   )
 }
